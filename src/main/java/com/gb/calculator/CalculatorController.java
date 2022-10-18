b23693ece5523b25cb632fffbef4cd8b26106d92
Alexandr <kb_299@bk.ru>
2022-10-18 11:33:15 +0300

    add memory function

--- src/main/java/com/gb/calculator/CalculatorController.java
+++ src/main/java/com/gb/calculator/CalculatorController.java
@@ -10,15 +10,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 
 
 public class CalculatorController extends Application {
     public TextField output;
+    public Label memoryOutput;
     boolean isResult = false;
+
+    Double finalResult = 0.0;
+    EvaluationValue result;
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("calculator.fxml")));
@@ -38,16 +45,20 @@ public class CalculatorController extends Application {
 
 
     public void inputResult(ActionEvent actionEvent) throws EvaluationException, ParseException {
-        try{
-            String text = output.getText();
-            Expression expression = new Expression(text);
-            EvaluationValue result = expression.evaluate();
-            Button button = (Button) actionEvent.getSource();
-            String digit = button.getText();
-            output.appendText(digit);
-            output.appendText(result.getStringValue());
-            isResult = true;
-        } catch (EvaluationException | ParseException e){
+        try {
+            if (isResult) {
+                cencel(actionEvent);
+            } else {
+                String text = output.getText();
+                Expression expression = new Expression(text);
+                result = expression.evaluate();
+                Button button = (Button) actionEvent.getSource();
+                String digit = button.getText();
+                output.appendText(digit);
+                output.appendText(result.getStringValue());
+                isResult = true;
+            }
+        } catch (EvaluationException | ParseException e) {
             output.clear();
             output.appendText("error");
             isResult = true;
@@ -56,6 +67,27 @@ public class CalculatorController extends Application {
 
     public void cencel(ActionEvent actionEvent) {
         output.clear();
+        result = null;
         isResult = false;
+    }
+
+    public void MCAction(ActionEvent actionEvent) {
+        memoryOutput.setText("");
+    }
+
+    public void MAction(ActionEvent actionEvent) {
+        if (memoryOutput.getText().equals("")) {
+            if (result == null){
+                memoryOutput.setText("M " + output.getText());
+            } else {
+                memoryOutput.setText("M " + result.getStringValue());
+            }
+        } else {
+            if (isResult) {
+                cencel(actionEvent);
+                output.appendText(memoryOutput.getText().substring(2));
+            }
+            output.appendText(memoryOutput.getText().substring(2));
+        }
     }
 }
